b7decdeb6d057a1e28e7e1653257271b75f665e7
freddie784 <83510059+freddie784@users.noreply.github.com>
2022-01-02 18:04:31 -0500

    started reviewing the basics

---
+++ Hello_input.py
@@ -0,0 +1,9 @@
+print("Hello, human! ")
+print('What is your name? ') #ask for name
+myName = input() #receive name
+print('It is good to meet you, ' + myName)
+print(f'The length of your name is {len(myName)} letters')
+print()
+print('How old are you? ')
+myAge = input() # Age is received as string
+print(f'You will be {int(myAge)+1} at some point this year!')
